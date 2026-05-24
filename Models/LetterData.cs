cc6c41ed024819b16f96c013e54669b11ca72328
Svyat-tut <slagor993@gmail.com>
2026-05-25 01:11:08 +0300

    Add LetterData class for letter generation

---
+++ Models/LetterData.cs
@@ -0,0 +1,14 @@
+namespace LetterGeneratorApp
+{
+    public class LetterData
+    {
+        public string RecipientName { get; set; } = "";
+        public string RecipientPost { get; set; } = "";
+        public string CompanyName { get; set; } = "";
+        public string LetterSubject { get; set; } = "";
+        public string LetterDate { get; set; } = "";
+        public string LetterBody { get; set; } = "";
+        public string SenderName { get; set; } = "";
+        public string SenderPost { get; set; } = "";
+    }
+}
